17275cda491558e462e9dd2e7d5fa2477e8a5231
whit <whit@openplans.org>
2009-02-06 13:58:04 -0600

    pavement default

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+try:
+    import paver.command
+except ImportError:
+    if os.path.exists("paver-minilib.zip"):
+        import sys
+        sys.path.insert(0, "paver-minilib.zip")
+    import paver.command
+paver.command.main()
 
-version = '0.0'
 
-setup(name='steamroller',
-      version=version,
-      description="tools to help with working with paver",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='disutils build',
-      author='whit',
-      author_email='whit@opengeo.org',
-      url='http://docs.opengeo.org',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
